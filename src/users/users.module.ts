71a2cf01c93a55b84d1cf9c0f92acc88ba78ffc2
joaosantos <joaovitor.d370@gmail.com>
2023-05-25 10:57:29 -0300

    first commit

---
+++ src/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './users.repository';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserRepository])],
+  providers: [UsersService, UserRepository],
+  controllers: [UsersController],
+})
+export class UsersModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { UserRepository } from './users.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
+import { UserRole } from './user-roles.enum';
+
+@Injectable()
+export class UsersService {
+  constructor(private userRepository: UserRepository) {}
+
+  async createAdminUser(createUserDto: CreateUserDto): Promise<User> {
+    if (createUserDto.password != createUserDto.passwordConfirmation) {
+      throw new UnprocessableEntityException('As senhas não conferem');
+    } else {
+      return this.userRepository.createUser(createUserDto, UserRole.ADMIN);
+    }
+  }
+}
